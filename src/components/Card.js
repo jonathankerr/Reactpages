e7091aa5949bc5e0b5751a3f0ba850ef5758d96c
jonathankerr <jonathankerr1@hotmail.co.uk>
2021-03-31 19:54:05 +0100

    First version of personal site

---
+++ src/components/Card.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import '../App.css';
+
+export default class Card extends Component {
+  render() {
+    return (
+        <div class="card-wrapper">
+            <h1>Jonathan Kerr</h1>
+            <ul>
+                <li>📍 Glasgow</li>
+                <li>🎓 The University of Strathclyde</li>
+                <li><a href="https://github.com/jonathankerr" target="_blank" rel="noreferrer">💻 Github</a></li>
+            </ul>
+        </div>
+    )
+  }
+}
