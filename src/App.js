c1e2aafbdc2d155735cd42b4e61b01a3886d1ed5
jonathankerr <jonathankerr1@hotmail.co.uk>
2021-03-31 18:10:55 +0100

    Added Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Home />
         <a
           className="App-link"
           href="https://reactjs.org"
